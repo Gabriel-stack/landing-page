9fe54c5269416b0a16b2b0b81ca6ac2df524ec08
Gabriel Alves <gabriel.alvesmartins98@gmail.com>
2021-07-16 16:58:16 -0300

    landing page desafio Hiring Coders

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let data ={
+        nome,
+        email
+    }
+    let convert = JSON.stringify(data);
+    localStorage.setItem('lead', convert);
+})
